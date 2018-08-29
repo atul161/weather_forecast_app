3dbab071a846991669e9d2965b3787cb57a9aab2
MATRIX <shivamedward@gmail.com>
2018-08-29 15:15:12 +0530

    drawer added and aligned

---
+++ lib/forecast/background/week_drawer.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class WeekDrawer extends StatelessWidget {
+
+  final week = [
+    'Tuesday\nAug27',
+    'Wednesday\nAug28',
+    'Thursday\nAug29',
+    'Friday\nAug30',
+    'Saturday\nAug31',
+    'Sunday\nSep01',
+    'Monday\nSep02',
+  ];
+
+  List<Widget> _buildDayButtons(){
+    return week.map((String title){
+      return new Expanded(
+        child: new GestureDetector(
+          onTap: (){
+            //TODO:
+          },
+          child: new Text(
+            title,
+            textAlign: TextAlign.center,
+            style: new TextStyle(
+              color: Colors.blue,
+              fontSize: 14.0,
+            ),
+          ),
+        ),
+      );
+    }).toList(); //it is needed to use this at the end of the map
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      width: 125.0,
+      height: double.infinity,
+      color: new Color(0xAA233030),
+      child: new Column(
+        children: <Widget>[
+          new Expanded(
+            child: new Icon(
+              Icons.refresh,
+              color: Colors.blue,
+              size: 40.0,
+            ),
+          ),
+        ]
+          ..addAll(_buildDayButtons(),),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:weather_forecast_app/forecast/background/app_bar.dart';
 import 'package:weather_forecast_app/forecast/background/background_with_rings.dart';
+import 'package:weather_forecast_app/forecast/background/week_drawer.dart';
 
 void main() => runApp(new MyApp());
 
@@ -32,13 +33,16 @@ class _MyHomePageState extends State<MyHomePage> {
         children: <Widget>[
           new BackgroundWithRings(),
 
-
           //next in the stack we are going to have app bar
           new Positioned( //position widgets are made for stack and they allow us to define the relationship the child widget has with the stack
             top: 0.0,
             left: 0.0,
             right: 0.0,
             child: new ForecastAppBar(),
+          ),
+          new Align(
+            alignment: Alignment.centerRight,
+              child: new WeekDrawer(),
           ),
         ],
       ),
